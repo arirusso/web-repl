36cd93edd864b91be41a58afbd66bc8cede2b1f7
Ari Russo <ari@paperlesspost.com>
2014-04-02 20:31:51 -0400

    add classes

--- lib/browser-repl.rb
+++ lib/browser-repl.rb
@@ -7,10 +7,16 @@ require "socket"
 
 # classes
 require "browser-repl/messager"
+require "browser-repl/patch"
 require "browser-repl/repl"
 
 module BrowserRepl
 
   VERSION = "0.1"
+
+  # Shortcut to REPL.start
+  def self.start(*a)
+    REPL.start(*a)
+  end
 
 end

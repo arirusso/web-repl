58abebd2435213daaeaef716f200b2cf38daf279
Ari Russo <ari@paperlesspost.com>
2014-04-02 20:32:01 -0400

    comments

--- lib/browser-repl/repl.rb
+++ lib/browser-repl/repl.rb
@@ -1,67 +1,72 @@
-# Patch EventMachine::WebSocket so that we can initialize EM on demand
-module EventMachine
-  module WebSocket
-    def self.start(options, &blk)
-      EM.epoll
-      #EM.run {
-        trap("TERM") { stop }
-        trap("INT")  { stop }
-
-        run(options, &blk)
-      #}
-    end
-
-    def self.run(options)
-      host, port = options.values_at(:host, :port)
-      EM.start_server(host, port, Connection, options) do |c|
-        yield c
-      end
-    end
-  end
-end
-
 module BrowserRepl
 
+  # The main REPL object
   class REPL
 
-    attr_reader :messager
+    # Start a repl connection
+    # @param [Hash] config A hash of config options to be passed to EM::WebSocket.run directly
+    # @param [Hash] options
+    # @option options [IO, nil] :debug A debug logger or nil if debug is not needed (default: nil)
+    # @option options [Boolean] :background Do not wait for input, just run in the bg
+    # @return [BrowserRepl::REPL]
+    def self.start(config, options = {})
+      new(config, options).tap { |repl| repl.start(options) }
+    end
 
-    def initialize(config)
+    # @param [Hash] config A hash of config options to be passed to EM::WebSocket.run directly
+    # @param [Hash] options
+    # @option options [IO, nil] :debug A debug logger or nil if debug is not needed (default: nil)
+    def initialize(config, options = {})
       @config = config
       @socket = nil
       @messager = nil
-      start
+      @debug = options[:debug]
     end
 
-    private
+    # Send a statement to the browser for evaluation
+    # @param [Fixnum, String] statement A Javascript statement to be evaluated
+    # @return [String, nil] The data that was sent to the browser, or nil if sending could not be completed. 
+    def evaluate(statement)
+      @messager.out({ :statement => statement }) unless @messager.nil?
+    end
 
-    def get_input
+    # Prompt the Ruby user for input and send that input to the browser for evaluation (blocking)
+    # @return [String, nil] The data that was sent to the browser, or nil if sending could not be completed
+    def gets
       line = Readline.readline('> ', true)
       return nil if line.nil?
       if line =~ /^\s*$/ or Readline::HISTORY.to_a[-2] == line
         Readline::HISTORY.pop
       end      
       statement = line.strip
-      if statement == "exit"
-        exit
-      else
-        @messager.out({ :statement => statement })
+      case statement
+        when "exit" then exit
+        else
+          evaluate(statement)
       end
     end
 
-    def start
+    # Start the Websocket connection (blocking)
+    # @param [Hash] options
+    # @option options [Boolean] :background Do not wait for input, just run in the bg
+    def start(options = {})
       EM::WebSocket.run(@config) do |ws|
         @socket = ws
         @messager = Messager.new(@socket)
-        configure
+        configure_event_handling(:background => options[:background])
       end
     end
 
-    def configure
+    private
+
+    # Configure the Websocket event handling
+    # @param [Hash] options
+    # @option options [Boolean] :background Do not wait for input, just run in the bg
+    def configure_event_handling(options = {})
       @socket.onopen do |handshake|
         puts "b-r: Connection open"
         @active = true
-        get_input
+        gets unless !!options[:background]
       end
 
       @socket.onclose do 
@@ -78,7 +83,7 @@ module BrowserRepl
           end
           puts(output)
         end
-        get_input
+        gets unless !!options[:background]
       end
 
     end

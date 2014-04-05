7df6ea757e2efc85c6b260a0b2d908eb5899feb7
Ari Russo <ari.russo@gmail.com>
2014-04-05 11:54:51 -0400

    comment

--- lib/web-repl.rb
+++ lib/web-repl.rb
@@ -10,6 +10,7 @@ require "web-repl/messager"
 require "web-repl/patch"
 require "web-repl/repl"
 
+# A Javascript REPL that runs in Ruby. Evaluation is done by a web browser instance.
 module WebRepl
 
   VERSION = "0.2"

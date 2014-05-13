ca226cb026a29692e0f07f676de16e28d9688a08
Ari Russo <ari.russo@gmail.com>
2014-05-12 22:38:50 -0400

    version bump

--- lib/web-repl.rb
+++ lib/web-repl.rb
@@ -13,7 +13,7 @@ require "web-repl/repl"
 # A Javascript REPL that runs in Ruby. Evaluation is done by a web browser instance.
 module WebRepl
 
-  VERSION = "0.4"
+  VERSION = "0.5"
 
   # Shortcut to REPL.start
   def self.start(*a)
